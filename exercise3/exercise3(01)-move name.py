c77fd40c888954814ce38dc6fe6a6e377c475efe
nasulong <243776262@qq.com>
2016-09-26 01:53:41 +0800

    Create exercise3(01)-move name.py

---
+++ exercise3/exercise3(01)-move name.py
@@ -0,0 +1,17 @@
+
+l = 70                                   #调节距离  
+t = 100                                  #调节时间  
+for a in range(t):  
+    v = int(a*l/t)                       #即a时刻的位移：x=a*v=a*l/t   
+    print  v*" "," #####    #     #         #"
+    print  v*" ","   #    #  #    # #     # # "
+    print  v*" ","   #   #    #   #  #   #  # "
+    print  v*" ","   #    #  #    #   # #   # "
+    print  v*" ","   #     #      #    #    # "
+    import os  
+    i = os.system('cls')  
+print  v*" "," #####    #     #         #"
+print  v*" ","   #    #  #    # #     # # "
+print  v*" ","   #   #    #   #  #   #  # "
+print  v*" ","   #    #  #    #   # #   # "
+print  v*" ","   #     #      #    #    # "
