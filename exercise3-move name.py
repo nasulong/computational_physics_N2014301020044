197c41ec72876c95898a59e3d44961d207048e24
nasulong <243776262@qq.com>
2016-09-26 00:20:56 +0800

    Update exercise3-move name.py

--- exercise3-move name.py
+++ exercise3-move name.py
@@ -1,20 +1,41 @@
+
 l = 70                                   #调节距离  
 t = 100                                  #调节时间  
 for a in range(t):  
     v = int(a*l/t)                       #即a时刻的位移：x=a*v=a*l/t   
-    print v*" ","pzhpz  pzhpzh pz  zh"
-    print v*" ","pz   h     zh pz  zh"
-    print v*" ","pz  zh    pz  pz  zh"
-    print v*" ","pzhpz    hp   pzhpzh"
-    print v*" ","pz      zh    pz  zh"
-    print v*" ","pz     pz     pz  zh"
-    print v*" ","pz     pzhpzh pz  zh"
+    print (v*" "," #####    #      #         #")
+    print (v*" ","   #    #  #    # #     # # ")
+    print (v*" ","   #   #    #   #  #   #  # ")
+    print (v*" ","   #    #  #    #   # #   # ")
+    print (v*" ","   #     #      #    #    # ")
     import os  
     i = os.system('cls')  
-print v*" ","pzhpz  pzhpzh pz  zh"
-print v*" ","pz   h     zh pz  zh"
-print v*" ","pz  zh    pz  pz  zh"
-print v*" ","pzhpz    hp   pzhpzh"
-print v*" ","pz      zh    pz  zh"
-print v*" ","pz     pz     pz  zh"
-print v*" ","pz     pzhpzh pz  zh"
+print (v*" "," #####    #      #         #")
+print (v*" ","   #    #  #    # #     # # ")
+print (v*" ","   #   #    #   #  #   #  # ")
+print (v*" ","   #    #  #    #   # #   # ")
+print (v*" ","   #     #      #    #    # ")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
